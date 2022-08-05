f0c8d3b8ab8b43a6bb2fccc52fd08a991de0896b
Aristide <dovi.aristide@gmail.com>
2022-08-05 16:13:57 +0000

    get genres

--- app.py
+++ app.py
@@ -245,7 +245,7 @@ def create_venue_submission():
     image_link = image_link,
     facebook_link = facebook_link)
 
-  print(request.form.data)
+  print(request.form.get('genres'))
 
   # on successful db insert, flash success
   flash('Venue ' + request.form['name'] + ' was successfully listed!')
