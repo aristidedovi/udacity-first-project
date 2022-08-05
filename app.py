1770672cccd8c61a7276c9f1b81c6365a704aaf3
Aristide <dovi.aristide@gmail.com>
2022-08-05 15:44:30 +0000

    update genres fields

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ class Venue(db.Model):
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-    genres = db.Column(db.Array())
+    genres = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
